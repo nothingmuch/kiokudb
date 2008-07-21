06f9dbc39bd452eb183d3bd862cc8c1235cae0be
Yuval Kogman <nothingmuch@woobling.org>
2008-07-21 19:09:52 +0000

    docs for Entry

--- lib/MooseX/Storage/Directory/Entry.pm
+++ lib/MooseX/Storage/Directory/Entry.pm
@@ -34,4 +34,48 @@ __END__
 
 =head1 NAME
 
-MooseX::Storage::Directory::Entry - 
+MooseX::Storage::Directory::Entry - An entry in the database
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+This object provides the meta data for a single storage entry.
+
+=head1 ATTRIBUTES
+
+=over 4
+
+=item id
+
+The UUID for the netry
+
+=item root
+
+Whether or not this is a member of the root set (not subject to garbage
+collection, because storage was explicitly requested).
+
+=item data
+
+A simplified data structure modeling this object/reference. This is a tree, not
+a graph, and has no shared data (JSON compliant). All references are symbolic,
+using a L<MooseX::Storage::Directory::Reference> object with UIDs as the
+address space.
+
+=item class
+
+If the entry is an object this contains the metaclass of that object.
+
+=back
+
+=head1 TODO
+
+=over 4
+
+=item *
+
+Model tiedness as a specialization of class?
+
+=back
+
+=cut
