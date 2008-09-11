59ce36c6c3c926b70d4123631766237ba70218f4
Yuval Kogman <nothingmuch@woobling.org>
2008-09-11 16:19:44 +0000

    GIN profiling in bench/small.pl

--- bench/small.pl
+++ bench/small.pl
@@ -17,6 +17,8 @@ my $mxsd_hash = KiokuDB->new(
     backend => KiokuDB::Backend::Hash->new,
 );
 
+my $q_employee = Search::GIN::Query::Class->new( class => "KiokuDB::Test::Employee" );
+
 sub bench_write {
     for ( 1 .. 20 ) {
         my $t = times;
@@ -41,7 +43,7 @@ sub bench_read {
     for ( 1 .. 20 ) {
         my $t = times;
         until ( times() - $t > 1 ) {
-            for ( 1 .. 250 ) { 
+            for ( 1 .. 250 ) {
                 circular_off($mxsd_hash->lookup(@ids));
             }
         }
@@ -52,5 +54,59 @@ sub bench_read {
     print "done\n";
 }
 
+sub bench_search {
+    use KiokuDB::GIN;
+    use KiokuDB;
+
+    use KiokuDB::Backend::Hash;
+
+    use Search::GIN::Query::Class;
+    use Search::GIN::Extract::Class;
+
+    {
+        package MyGIN;
+        use Moose;
+
+        extends qw(KiokuDB::Backend::Hash);
+
+        with (
+            qw(
+            KiokuDB::GIN
+            Search::GIN::Driver::Hash
+            Search::GIN::Extract::Delegate
+            ),
+        );
+
+        __PACKAGE__->meta->make_immutable;
+    }
+
+    my $gin = MyGIN->new(
+        extract => Search::GIN::Extract::Class->new,
+        root_only => 0,
+    );
+
+    my $dir = KiokuDB->new(
+        backend => $gin,
+    );
+
+    $dir->store($f->create) for 1 .. 10;
+
+    my $q_employee = Search::GIN::Query::Class->new( class => "KiokuDB::Test::Employee" );
+
+    for ( 1 .. 20 ) {
+        my $t = times;
+        until ( times() - $t > 1 ) {
+            for ( 1 .. 10 ) {
+                $dir->search($q_employee);
+            }
+        }
+        print ".";
+        print " " if $_ % 5 == 0;
+    }
+
+    print "done\n";
+}
+
 #bench_read();
-bench_write();
+#bench_write();
+bench_search();
