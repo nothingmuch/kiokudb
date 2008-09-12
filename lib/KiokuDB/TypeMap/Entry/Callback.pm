53cebf66438dea768e01856f50d7c551af8e0b6b
Yuval Kogman <nothingmuch@woobling.org>
2008-09-12 00:42:50 +0000

    Callback typemap entry

--- lib/KiokuDB/TypeMap/Entry/Callback.pm
+++ lib/KiokuDB/TypeMap/Entry/Callback.pm
@@ -3,18 +3,46 @@
 package KiokuDB::TypeMap::Entry::Callback;
 use Moose;
 
+use Data::Swap qw(swap);
+
 use namespace::clean -except => 'meta';
 
 with qw(KiokuDB::TypeMap::Entry::Std);
 
 has [qw(collapse expand)] => (
+    is  => "ro",
     isa => "Str|CodeRef",
     required => 1,
 );
 
 sub compile_mappings {
     my ( $self, @args ) = @_;
-    return $self->collapse, $self->expand;
+
+    my $collapse_object = $self->collapse;
+    my $collapse = sub {
+        my ( $self, %args ) = @_;
+
+        return [ map { $self->visit($_) } $args{object}->$collapse_object() ];
+    };
+
+    my $expand_object = $self->expand;
+    my $expand = sub {
+        my ( $self, $entry ) = @_;
+
+        # FIXME see Linker::expand_naive
+
+        my $placeholder = {};
+        $self->register_object( $entry => $placeholder );
+
+        my @args = map { $self->inflate_data($_) } @{ $entry->data };
+
+        my $object = $entry->class->$expand_object(@args);
+
+        swap($object, $placeholder);
+        return $placeholder;
+    };
+
+    return ( $collapse, $expand );
 }
 
 __PACKAGE__->meta->make_immutable;
