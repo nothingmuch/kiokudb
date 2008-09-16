017504bdc45662f79cd5874d6c3223ddec220ce6
Yuval Kogman <nothingmuch@woobling.org>
2008-09-16 08:02:57 +0000

    compare txn vs. non txn bdb

--- bench/shootout.pl
+++ bench/shootout.pl
@@ -41,9 +41,19 @@ sub bench {
         ),
     );
 
-    my $mxsd_bdb = KiokuDB->new(
+    my $mxsd_bdb_txn = KiokuDB->new(
         backend => KiokuDB::Backend::BDB->new(
-            dir => $dir->subdir("mxsd_bdb"),
+            dir => $dir->subdir("mxsd_bdb_txn"),
+        ),
+    );
+
+    $dir->subdir("mxsd_bdb_dumb")->mkpath;
+    my $mxsd_bdb_dumb = KiokuDB->new(
+        backend => KiokuDB::Backend::BDB->new(
+            manager => BerkeleyDB::Manager->new(
+                home => $dir->subdir("mxsd_bdb_dumb"),
+                transactions => 0,
+            ),
         ),
     );
 
@@ -72,29 +82,35 @@ sub bench {
 
     warn "\nwriting...\n";
 
-    cmpthese(-1, {
+    $mxsd_bdb_txn->backend->txn_do(sub {
+
+    cmpthese(-2, {
         null       => sub { my @objs = construct(); },
         mxsd_hash  => sub { my @objs = construct(); my $s = $mxsd_hash->new_scope; $mxsd_hash->store(grep { blessed($_) } @objs) },
         mxsd_jspon => sub { my @objs = construct(); my $s = $mxsd_jspon->new_scope; $mxsd_jspon->store(grep { blessed($_) } @objs) },
-        mxsd_bdb   => sub { my @objs = construct(); my $s = $mxsd_bdb->new_scope; $mxsd_bdb->store(grep { blessed($_) } @objs) },
+        mxsd_bdb   => sub { my @objs = construct(); my $s = $mxsd_bdb_dumb->new_scope; $mxsd_bdb_dumb->store(grep { blessed($_) } @objs) },
+        mxsd_bdb_txn   => sub { my @objs = construct(); my $s = $mxsd_bdb_txn->new_scope; $mxsd_bdb_txn->store(grep { blessed($_) } @objs) },
         ( $mxsd_couch ? ( mxsd_couch => sub { my @objs = construct(); my $s = $mxsd_couch->new_scope; $mxsd_couch->store(grep { blessed($_) } @objs) } ) : () ),
         storable   => sub { nstore([ construct() ], $storable) },
     });
 
-    warn "\nreading...\n";
+    });
 
+    warn "\nreading...\n";
 
     my @hash_ids  = do { my @objs = construct(); my $s = $mxsd_hash->new_scope; $mxsd_hash->store(grep { blessed($_) } @objs) };
     my @jspon_ids = do { my @objs = construct(); my $s = $mxsd_jspon->new_scope; $mxsd_jspon->store(grep { blessed($_) } @objs) };
-    my @bdb_ids   = do { my @objs = construct(); my $s = $mxsd_bdb->new_scope; $mxsd_bdb->store(grep { blessed($_) } @objs) };
+    my @bdb_d_ids = do { my @objs = construct(); my $s = $mxsd_bdb_dumb->new_scope; $mxsd_bdb_dumb->store(grep { blessed($_) } @objs) };
+    my @bdb_t_ids = do { my @objs = construct(); my $s = $mxsd_bdb_txn->new_scope; $mxsd_bdb_txn->backend->txn_do(sub { $mxsd_bdb_txn->store(grep { blessed($_) } @objs) }); };
     my @couch_ids = $mxsd_couch ? do { my @objs = construct(); my $s = $mxsd_couch->new_scope; $mxsd_couch->store(grep { blessed($_) } @objs) } : ();
 
-    cmpthese(-1, {
-        mxsd_hash  => sub { my $s = $mxsd_hash->new_scope; my @objs = $mxsd_hash->lookup(@hash_ids) },
-        mxsd_jspon => sub { my $s = $mxsd_jspon->new_scope; my @objs = $mxsd_jspon->lookup(@jspon_ids) },
-        mxsd_bdb   => sub { my $s = $mxsd_bdb->new_scope; my @objs = $mxsd_bdb->lookup(@bdb_ids) },
+    cmpthese(-2, {
+        storable     => sub { my $objs = retrieve($storable) },
+        mxsd_hash    => sub { my $s = $mxsd_hash->new_scope; my @objs = $mxsd_hash->lookup(@hash_ids) },
+        mxsd_jspon   => sub { my $s = $mxsd_jspon->new_scope; my @objs = $mxsd_jspon->lookup(@jspon_ids) },
+        mxsd_bdb     => sub { my $s = $mxsd_bdb_dumb->new_scope; my @objs = $mxsd_bdb_dumb->lookup(@bdb_d_ids) },
+        mxsd_bdb_txn => sub { my $s = $mxsd_bdb_txn->new_scope; my @objs = $mxsd_bdb_txn->lookup(@bdb_t_ids) },
         ( $mxsd_couch ? ( mxsd_couch => sub { my $s = $mxsd_couch->new_scope; my @objs = $mxsd_couch->lookup(@couch_ids) } ) : () ),
-        storable   => sub { my $objs = retrieve($storable) },
     });
 }
 
