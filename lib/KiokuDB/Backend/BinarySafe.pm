c1789931b28f6f87ca4f6fea66046b7d4deee80b
Yuval Kogman <nothingmuch@woobling.org>
2008-10-02 23:43:55 +0000

    UnicodeSafe and BinarySafe role docs

--- lib/KiokuDB/Backend/BinarySafe.pm
+++ lib/KiokuDB/Backend/BinarySafe.pm
@@ -10,3 +10,29 @@ use namespace::clean -except => 'meta';
 __PACKAGE__
 
 __END__
+
+=pod
+
+=head1 NAME
+
+KiokuDB::Backend::BinarySafe - An informational role for binary data safe
+backends.
+
+=head1 SYNOPSIS
+
+    package KiokuDB::Backend::MySpecialBackend;
+    use Moose;
+
+    use namespace::clean -except => 'meta';
+
+    with qw(KiokuDB::Backend::BinarySafe);
+
+=head1 DESCRIPTION
+
+This backend is an informational role for backends which can store arbitrary
+binary strings.
+
+This mostly has to do with L<KiokuDB::Backend::Serialize> variants (e.g.
+L<Storable> based serialization is binary safe, while
+L<KiokuDB::Backend::Serialize::JSPON> is not).
+
