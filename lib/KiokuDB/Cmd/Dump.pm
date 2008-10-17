081151bd0bc8dc9048afc562328abbb615c72c45
Yuval Kogman <nothingmuch@woobling.org>
2008-10-17 12:08:57 -0700

    docs for Dump

--- lib/KiokuDB/Cmd/Dump.pm
+++ lib/KiokuDB/Cmd/Dump.pm
@@ -198,13 +198,105 @@ __END__
 
 =head1 NAME
 
-KiokuDB::Cmd::Dump - 
+KiokuDB::Cmd::Dump - Dump database entries for backup or munging purposes
 
 =head1 SYNOPSIS
 
+    # command line API
+    # dump whole database to foo.yml sequentially
+
+    % kiokudump --verbose --file foo.yml --format yaml --backup --dsn bdb-gin:dir=data/
+
+
+
+    # programmatic API
+
 	use KiokuDB::Cmd::Dump;
 
+    my $dumper = KiokuDB::Cmd::Dump->new(
+        backend => $backend,
+        formatter => sub { ... },
+        output_handle => $fh,
+    );
+
+    $dumper->run;
+
 =head1 DESCRIPTION
+
+Using the L<KiokuDB::Backend::Scan> interface, any supporting backend can be
+dumped using this api.
+
+The data can then be edited or simply retained for backup purposes.
+
+The data can be loaded using L<KiokuDB::Backend::Load>.
+
+=head1 COMMAND LINE API
+
+This class uses L<MooseX::Getopt> to provide a command line api.
+
+The command line options map to the class attributes.
+
+=head1 METHODS
+
+=over 4
+
+=item new_with_options
+
+Provided by L<MooseX::Getopt>. Parses attributes init args from C<@ARGV>.
+
+=item run
+
+Performs the actual dump.
+
+=back
+
+=head1 ATTRIBUTES
+
+=over 4
+
+=item dsn
+
+=item backend
+
+The backend to be dumped.
+
+C<dsn> is a string and thus can be used on the command line. C<backend> is
+defined in terms of C<dsn> if it isn't provided.
+
+=item format
+
+=item formatter
+
+C<format> is one of C<yaml>, C<storable> or C<json>, defaulting to C<yaml>.
+
+C<formatter> is a code reference which takes an entry as an argument. It is
+created from a set of defaults using C<format> if it isn't provided.
+
+=item file
+
+=item backup
+
+=item force
+
+=item backup_ext
+
+=item output_handle
+
+C<output_handle> is where the returned string of C<formatter> will be printed.
+
+If it isn't provided, C<file> will be opened for writing. If the file already
+exists and C<force> is specified, it will be overwritten. If C<backup> is
+provided the original will first be renamed using C<backup_ext> (defaults to
+C<.bak>). If the backup already exists, then C<force> will allow overwriting of
+the previous backup.
+
+If no file is provided then C<STDOUT> is used.
+
+=item verbose
+
+If enabled causes progress information to be printed to C<STDERR>.
+
+=back
 
 =cut
 
