a227f101c3ec920cb745992cf5d1e32b38ca0699
Yuval Kogman <nothingmuch@woobling.org>
2008-10-17 12:26:10 -0700

    docs for Load

--- lib/KiokuDB/Cmd/Load.pm
+++ lib/KiokuDB/Cmd/Load.pm
@@ -231,13 +231,103 @@ __END__
 
 =head1 NAME
 
-KiokuDB::Cmd::Load - 
+KiokuDB::Cmd::Load - Load database dumps
 
 =head1 SYNOPSIS
 
+    # command line API
+    # dump whole database to foo.yml sequentially
+
+    % kiokuload --verbose --file foo.yml --format yaml --clear --dsn bdb-gin:dir=data/
+
+    
+    # programmatic API
+
 	use KiokuDB::Cmd::Load;
 
+    my $loader = KiokuDB::Cmd::Load->new(
+        backend => $backend,
+        formatter => sub { ... },
+        input_handle => $fh,
+    );
+
+    $dumper->run;
+
+
 =head1 DESCRIPTION
+
+This class loads dumps created by L<KiokuDB::Cmd::Dump>.
+
+Entries will be read sequentially from C<input_handle>, deserialized, and
+inserted into the database.
+
+If the backend supports L<KiokuDB::Backend::TXN> then the load is performed in
+a single transaction.
+
+=head1 COMMAND LINE API
+
+This class uses L<MooseX::Getopt> to provide a command line api.
+
+The command line options map to the class attributes.
+
+=head1 METHODS
+
+=over 4
+
+=item new_with_options
+
+Provided by L<MooseX::Getopt>. Parses attributes init args from C<@ARGV>.
+
+=item run
+
+Performs the actual load.
+
+=back
+
+=head1 ATTRIBUTES
+
+=over 4
+
+=item dsn
+
+=item backend
+
+The backend to be dumped.
+
+C<dsn> is a string and thus can be used on the command line. C<backend> is
+defined in terms of C<dsn> if it isn't provided.
+
+=item format
+
+=item formatter
+
+C<format> is one of C<yaml>, C<storable> or C<json>, defaulting to C<yaml>.
+
+C<formatter> is a code reference that is provided with a file handle and should
+return at least one entry object.
+
+It is applied to the handle repeatedly until no more entries are returned.
+
+=item clear
+
+If set, L<KiokuDB::Backend::Clear>'s interface will be used to wipe the
+database before loading.
+
+=item file
+
+=item input_handle
+
+C<input_handle> is where entries will be read from.
+
+If it isn't provided and then C<file> will be opened for reading.
+
+If C<file> isn't provided C<STDIN> will be used.
+
+=item verbose
+
+If enabled causes progress information to be printed to C<STDERR>.
+
+=back
 
 =cut
 
