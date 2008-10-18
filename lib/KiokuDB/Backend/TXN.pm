16c1932209cb4f0a51049eb3a4c9fe3b1c54f153
Yuval Kogman <nothingmuch@woobling.org>
2008-10-18 02:15:58 -0700

    doc stub for TXN

--- lib/KiokuDB/Backend/TXN.pm
+++ lib/KiokuDB/Backend/TXN.pm
@@ -10,3 +10,32 @@ requires qw(txn_begin txn_commit txn_rollback);
 __PACKAGE__
 
 __END__
+
+=pod
+
+=head1 NAME
+
+KiokuDB::Backend::TXN - Backend level transaction support.
+
+=head1 SYNOPSIS
+
+    package MyBackend;
+    use Moose;
+
+    with qw(
+        KiokuDB::Backend
+        KiokuDB::Backend::TXN
+    );
+
+    sub txn_begin { ... }
+    sub txn_commit { ... }
+    sub txn_rollback { ... }
+
+=head1 DESCRIPTION
+
+This API is inspired by standard database transactions much like you get with
+L<DBI>.
+
+This is the low level interface required by L<KiokuDB/txn_do>.
+
+=cut
