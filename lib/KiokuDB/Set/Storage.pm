b200788c77d3630a9113b88471691cda75c49c17
Yuval Kogman <nothingmuch@woobling.org>
2008-10-25 14:42:23 -0700

    docs for KiokuDB::Set::Stored and KiokuDB::Set::Storage

--- lib/KiokuDB/Set/Storage.pm
+++ lib/KiokuDB/Set/Storage.pm
@@ -12,3 +12,20 @@ with qw(KiokuDB::Set);
 __PACKAGE__
 
 __END__
+
+=pod
+
+=head1 NAME
+
+KiokuDB::Set::Storage - Role for L<KiokuDB::Set>s that are tied to storage.
+
+=head1 SYNOPSIS
+
+    # informational role, used internally
+
+=head1 DESCRIPTION
+
+This role is informational, and implemented by L<KiokuDB::Set::Deferred> and
+L<KiokuDB::Set::Loaded>
+
+=cut
--- lib/KiokuDB/Set/Stored.pm
+++ lib/KiokuDB/Set/Stored.pm
@@ -14,3 +14,20 @@ __PACKAGE__->meta->make_immutable;
 __PACKAGE__
 
 __END__
+
+=pod
+
+=head1 NAME
+
+KiokuDB::Set::Stored - Stored representation of L<KiokuDB::Set> objects.
+
+=head1 SYNOPSIS
+
+    # used internally by L<KiokuDB::TypeMap::Entry::Set>
+
+=head1 DESCRIPTION
+
+This object is the persisted representation of all L<KiokuDB::Set> objects.
+
+It is used internall after collapsing and before expanding, for simplicity.
+
