b0867fd13ff0702c753c846e337f3f1d2aa1332d
Rob Hoelz <rhoelz@inoc.com>
2010-10-04 13:25:54 -0500

    Add docs for KiokuDB::TypeMap::Entry::Compiled

--- lib/KiokuDB/TypeMap/Entry/Compiled.pm
+++ lib/KiokuDB/TypeMap/Entry/Compiled.pm
@@ -28,3 +28,59 @@ __PACKAGE__->meta->make_immutable;
 __PACKAGE__
 
 __END__
+
+=pod
+
+=head1 NAME
+
+KiokuDB::TypeMap::Entry::Compiled - Object for storing collapse/expand methods
+
+=head1 SYNOPSIS
+
+TODO
+
+=head1 DESCRIPTION
+
+Objects of this class should be returned by L<KiokuDB::TypeMap::Entry/compile>.
+You probably shouldn't be using this directly; you may just want to consume
+L<KiokuDB::TypeMap::Entry::Std> or something.
+
+=head1 ATTRIBUTES
+
+=over 4
+
+=item expand_method
+
+Contains a subroutine reference (or a string, denoting a method name).  It is
+called as method on the L<KiokuDB::Linker>.  Takes a L<KiokuDB::Entry> as an
+argument, and should return the expanded object.
+
+=item collapse_method
+
+Contains a subroutine reference (or a string, denoting a method name).  It is
+called as method on the L<KiokuDB::Collapser>.  Takes the object to 
+be collapsed as an argument, and should return a L<KiokuDB::Reference>.
+
+=item id_method
+
+Contains a subroutine reference (or a string, denoting a method name).  It is
+called as method on the L<KiokuDB::Collapser>.  Takes the object to be
+collapsed as an argument, and should return an ID for it .
+
+=item refresh_method
+
+Contains a subroutine reference (or a string, denoting a method name).  It is
+called as method on the L<KiokuDB::Linker>.  Takes the object to be refreshed
+and its corresponding L<KiokuDB::Entry> as arguments.
+
+=item class
+
+The class for which the methods are being compiled.
+
+=item entry
+
+The L<KiokuDB::TypeMap::Entry> that created this object.
+
+=back
+
+=cut
